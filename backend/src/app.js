5b7cef1331ef70ea6c515fd8ddf5e4c13551a906
RTXOVER <lgfflguga@gmail.com>
2025-11-22 01:57:07 -0300

    feat: add CRUD APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,11 +6,16 @@ app.use(express.json());
 
 
 //routes import
-import userRouter from "./routes/user.route.js";
+import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.routes.js';
+
 
 // routes declaration
 app.use ("/api/v1/users", userRouter);
+app.use ("/api/v1/posts", postRouter);
 
 //example route: http://localhost:4000/api/v1/users/register
+//example route: http://localhost:4000/api/v1/posts/create
+
 
 export default app;
