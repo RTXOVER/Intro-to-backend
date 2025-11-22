1d5b58cddebe18ec747902eb1e4f095a2261b84d
RTXOVER <lgfflguga@gmail.com>
2025-11-22 02:08:39 -0300

    feat: get post by id

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -37,8 +37,25 @@ const getPosts = async (req, res) => {
         });
         
     }
+}
 
+// Read id posts
+const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
 
+        if (!post) 
+            return res.status(404).json({
+                 message: "Post not found"
+             });
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error", error
+        });
+        
+    }
 }
 
 const updatePost = async (req, res) => {
@@ -88,6 +105,7 @@ const deletePost = async (req, res) => {
 export { 
 createPost,
 getPosts,
+getPost,
 updatePost,
 deletePost
 };
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createPost, deletePost, getPosts, updatePost } from '../controllers/post.controller.js';
+import { createPost, getPosts, getPost, updatePost, deletePost } from '../controllers/post.controller.js';
 
 const router = Router();
 
 router.route('/create').post(createPost);
 router.route('/getPosts').get(getPosts);
+router.route('/getPost/:id').get(getPost);
 router.route('/update/:id').patch(updatePost);
 router.route('/delete/:id').delete(deletePost);
 
